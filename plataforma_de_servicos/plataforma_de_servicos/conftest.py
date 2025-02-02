3ffd12c177770ca36f85c58723d90d66d57fc3a5
juanLadeira <ladeirajuan96@gmail.com>
2025-02-02 18:51:50 -0300

    feat: commit inicial do projeto

---
+++ plataforma_de_servicos/plataforma_de_servicos/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from plataforma_de_servicos.users.models import User
+from plataforma_de_servicos.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ plataforma_de_servicos/plataforma_de_servicos/users/tests/test_models.py
@@ -0,0 +1,5 @@
+from plataforma_de_servicos.users.models import User
+
+
+def test_user_get_absolute_url(user: User):
+    assert user.get_absolute_url() == f"/users/{user.pk}/"
